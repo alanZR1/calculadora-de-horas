1f982858e51eda33cb5c9fbf32e71fdfb35ab117
Alan Reyes Luis <alannreyes02@gmail.com>
2025-05-14 18:06:31 -0600

    importando nuevas funcionalidades de logic.py

--- calculadoraHoras/main.py
+++ calculadoraHoras/main.py
@@ -1,13 +1,20 @@
 import flet as ft
-from calculadoraHoras.logic import (cargar_cronograma,
+from logic import (cargar_cronograma,
     guardar_cronograma, calcular_horas_en_periodo,
     agregar_actividad, eliminar_actividad, modificar_actividad)
-from calculadoraHoras.ui_components import crear_interfaz
+from ui_components import crear_interfaz
 
 def main(page: ft.Page):
+    page.window_width = 500
+    page.window_height = 400
+    page.window_min_width = 300
+    page.window_min_height = 400
+    
     page.title = "Calculadora de Horas de Servicio Social"
     page.vertical_alignment = ft.MainAxisAlignment.START
     page.scroll = ft.ScrollMode.AUTO
+    page.theme_mode = ft.ThemeMode.DARK  # o DARK/SYSTEM
+    
 
     # Cargar cronograma al iniciar
     cronograma = cargar_cronograma()
@@ -21,7 +28,7 @@ def main(page: ft.Page):
         eliminar_actividad,
         modificar_actividad
     )
-    page.add(*interfaz)
+    page.add(interfaz)
 
 if __name__ == "__main__":
     ft.app(target=main)  # Ejecuta como app de escritorio
